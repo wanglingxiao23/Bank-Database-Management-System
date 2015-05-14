8529c83c4a82a26fe2882bb3f7e67a8f1ad9286d
benlingxiao <benlingxiao@163.com>
2015-05-14 19:29:08 +0800

    20150514,用session解决登陆问题，值得记录一下
    Signed-off-by: benlingxiao <benlingxiao@163.com>

--- routes/index.js
+++ routes/index.js
@@ -1,76 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var conn = require('db');
-
-//生成一个session//
-var sessions = {};
-var key = 'session_id';
-var EXPIRES = 20 * 60 * 1000;
-var generate = function(){
-    var session = {};
-    session.id = (new Date()).getTime() + Math.random();
-    session.cookie = {
-        expire: (new Date()).getTime() + EXPIRES
-    };
-    sessions[session.id] = session;
-    return session;
-};
-//============//
-/*function (req,res){
-    //====检测session===//
-    var id = req.cookies[key];
-    if(!id){
-        req.session = generate();
-    }else{
-        var session = session[id];
-        if(session){
-            if(session.cookie.expire>(new Date()).getTime()){
-                //更新超时时间
-                session.cookie.expire = (new Date()).getTime() + EXPIRES;
-                req.session = session;
-            }else{
-                //超时了，删除旧的数据，并重新生成
-                delete sessions[id];
-                req.session = generate();
-            }
-        }else{
-            //如果session过期或口令不对，重新生成session
-            req.session = generate();
-        }
-    }
-}*/
-
+var session = require('express-session');
+var flash = require('connect-flash');
 /* GET home page. */
-router.get('/',function(req,res,next){
-    //====检测session===//
-    var id = req.cookies[key];
-    console.log(id);
-    if(!id){
-        req.session = generate();
-    }else{
-        var session = session[id];
-        if(session){
-            if(session.cookie.expire>(new Date()).getTime()){
-                //更新超时时间
-                session.cookie.expire = (new Date()).getTime() + EXPIRES;
-                req.session = session;
-            }else{
-                //超时了，删除旧的数据，并重新生成
-                delete sessions[id];
-                req.session = generate();
-            }
-        }else{
-            //如果session过期或口令不对，重新生成session
-            req.session = generate();
-        }
-    }
-    console.log(req.session);
-    if (!req.session.adminid) {
-        res.redirect('/login');
-    }else{
-        var adminid = req.session.adminid;
+router.get('/',checkLogin);
+router.get('/',function(req,res){
+        var adminid = session.adminid;
         console.log(adminid);
-    }
+        res.render('');
 });
 
 
@@ -80,27 +18,6 @@ router.get('/login', function(req, res, next) {
 });
 
 router.post('/login/admin',function(req,res,next) {
-    //session检测
-    var id = req.cookies[key];
-    if(!id){
-        req.session = generate();
-    }else{
-        var session = session[id];
-        if(session){
-            if(session.cookie.expire>(new Date()).getTime()){
-                //更新超时时间
-                session.cookie.expire = (new Date()).getTime() + EXPIRES;
-                req.session = session;
-            }else{
-                //超时了，删除旧的数据，并重新生成
-                delete sessions[id];
-                req.session = generate();
-            }
-        }else{
-            //如果session过期或口令不对，重新生成session
-            req.session = generate();
-        }
-    };
     //===handle====//
     var adminTel = req.body.admintel;
     var adminPsd = req.body.adminpsd;
@@ -115,9 +32,8 @@ router.post('/login/admin',function(req,res,next) {
                 res.send(200,{data:1});
             } else {
                 var adminid = results[0].adminid;
-                req.session.adminid = adminid;
-                console.log(req.session);
-                //res.setHeader('Set-Cookie','"adminid"="' + adminid+'"')
+                session.adminid = adminid;
+                console.log(session);
                 res.send(200,{data:0});
             }
         });
@@ -195,5 +111,12 @@ router.get('/404', function(req, res, next) {
     res.render('404');
 });
 
+function checkLogin(req, res, next) {
+    if (session.adminid) {
+        flash('error', '未登录!');
+        res.redirect('/login');
+    }
+    next();
+}
 
 module.exports = router;
